bcb191374bc611e82bd5d4a7d961915782016d20
Backk <erickfx45@gmail.com>
2025-07-17 02:46:43 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ function App() {
               {/* Large image placeholder with glass morphism frame */}
               <div className="relative w-96 h-96 backdrop-blur-xl bg-white/10 border border-white/20 rounded-3xl p-4 shadow-2xl">
                 <img 
-                  src="https://imgur.com/1byCoqA"
+                  src="https://i.imgur.com/1byCoqA.png"
                   alt="Hero Image"
                   className="w-full h-full object-cover rounded-2xl"
                 />

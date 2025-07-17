cfa5d6c59f42e9b60cfb9ea3582403537d3511c1
Backk <erickfx45@gmail.com>
2025-07-17 11:10:43 -0300

    Updated videos.ts

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,4 +1,12 @@
 export const videos = [
+    {
+    id: "o6q6oWERX3s",
+    title: "Ronaldenho",
+    thumbnail: "https://i9.ytimg.com/vi/bMKGWxg2jec/mqdefault.jpg?sqp=CJzet8MG-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGEwgVShlMA8=&rs=AOn4CLCLM2r_NTjnpvSr1Q8j3nq6mTgKQQ",
+    thumbnailstatic: "https://i9.ytimg.com/vi/bMKGWxg2jec/mqdefault.jpg?sqp=CJzet8MG-oaymwEmCMACELQB8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGEwgVShlMA8=&rs=AOn4CLCLM2r_NTjnpvSr1Q8j3nq6mTgKQQ",
+    category: "motion",
+    description: ""
+  },
   {
     id: "bMKGWxg2jec",
     title: "Ronaldenho",
